2173d91a2b70b241a9a63343cab90870b71b425b
KevinChiguano <kevin_chili1998@hotmail.com>
2023-07-13 13:00:47 -0500

    Servicio Materia

---
+++ src/main/java/com/example/demo/repository/modelo/Materia.java
@@ -0,0 +1,74 @@
+package com.example.demo.repository.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "materia")
+public class Materia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_materia")
+	@SequenceGenerator(name = "seq_materia", sequenceName = "seq_materia", allocationSize = 1)
+	@Column(name = "mate_id")
+	private Integer id;
+	
+	@Column(name = "mate_nombre")
+	private String nombre;
+	
+	@Column(name = "mate_codigo")
+	private String codigo;
+	
+	@Column(name = "mate_creditos")
+	private Double creditos;
+	
+	@Column(name = "mate_descripcion")
+	private String descripcion;
+
+	//SET y GET
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Double getCreditos() {
+		return creditos;
+	}
+
+	public void setCreditos(Double creditos) {
+		this.creditos = creditos;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
